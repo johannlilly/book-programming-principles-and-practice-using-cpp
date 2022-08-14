648e696539401e04e54678f1b9b84f23ce2c9627
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2022-08-14 11:03:01 -0400

    Create find_upstairs_bathroom.cpp

---
+++ practice/ch02/exercises/find_upstairs_bathroom.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+inline void keep_window_open() { char ch; cin>>ch; }
+
+int main()
+{
+  cout << "Find the upstairs bathroom.\n";
+  
+  keep_window_open();
+  
+  return 0;
+}
