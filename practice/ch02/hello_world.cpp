ae3e2eeed7d1af00ec517a8ef8e56c8cbbb95434
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2022-08-14 10:32:59 -0400

    Update hello_world.cpp
    If you have no internet access

--- practice/ch02/hello_world.cpp
+++ practice/ch02/hello_world.cpp
@@ -1,4 +1,10 @@
-#include "std_lib_facilities.h"
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+inline void keep_window_open() { char ch; cin>>ch; }
 
 int main()
 {
