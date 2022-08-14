746be0eddb62bbbdb63bcf90d567077d9218c160
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2022-08-14 11:00:04 -0400

    Create hello_world_2.cpp

---
+++ practice/ch02/exercises/hello_world_2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+inline void keep_window_open() { char ch; cin>>ch; }
+
+int main()
+{
+  cout << "Hello, World!\n";
+  // Change the program to output the two lines
+  cout << "Here we go!\n";
+  
+  keep_window_open();
+  
+  return 0;
+}
